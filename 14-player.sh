e7efdefad2181df00ecdfc8dacf39aa574f8eed3
lecramyajiv <36596722+lecramyajiv@users.noreply.github.com>
2020-12-31 21:54:29 +0530

    Create 14-player.sh

---
+++ 14-player.sh
@@ -0,0 +1,6 @@
+# This needs to run as root
+# Install subdownloader if you need it
+
+#!/bin/bash
+
+apt-get --no-install-recommends --no-install-suggests install mpv smplayer smplayer-themes vlc vlc-plugin-access-extra vlc-plugin-notify vlc-plugin-visualization streamtuner2 audacity audacious audacious-plugins audacious-plugins-data winff
