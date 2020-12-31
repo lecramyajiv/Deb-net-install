e0cb67474947fbc2257a0ae7d020bede591cd6e7
lecramyajiv <36596722+lecramyajiv@users.noreply.github.com>
2020-12-31 22:12:51 +0530

    Create 15-system.sh

---
+++ 15-system.sh
@@ -0,0 +1,7 @@
+# This needs to run as root
+#doublecmd-qt and worker filemanager
+
+
+#!/bin/bash
+
+apt-get --no-install-recommends --no-install-suggests install acetoneiso synaptic gparted xarchiver spacefm-gtk3 porg
