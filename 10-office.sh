98886f6a7b78b94f2f287e526b9850874bf3da28
lecramyajiv <36596722+lecramyajiv@users.noreply.github.com>
2020-12-31 21:23:18 +0530

    Create 10-office.sh

---
+++ 10-office.sh
@@ -0,0 +1,6 @@
+# This needs to run as root
+# libreoffice-gnome and libreoffice-gtk3 needs to be installed for ibus to work properly in libreoffice
+
+#!/bin/bash
+
+apt-get --no-install-recommends --no-install-suggests install qpdfview qpdfview-djvu-plugin qpdfview-pdf-poppler-plugin qpdfview-ps-plugin qpdfview-translations xchm bookworm vym pdfarranger libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-gtk3 libreoffice-gnome hunspell-en-au hunspell-en-ca hunspell-en-gb hunspell-en-med hunspell-en-us hunspell-en-za mythes-en-au mythes-en-us hyphen-en-gb hyphen-en-us
