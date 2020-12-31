9c88e30ba7b651b53630a58c4017cc1a510b68c3
lecramyajiv <36596722+lecramyajiv@users.noreply.github.com>
2020-12-31 22:16:55 +0530

    Update and rename 05-app.sh to 05- gir.sh

---
+++ 05- gir.sh
@@ -0,0 +1,11 @@
+# This needs to run as root
+# Install after installing xorg but before DE or any window manager
+
+
+#!/bin/bash
+
+apt-get --no-install-recommends --no-install-suggests install gir1.2-atk-1.0 gir1.2-freedesktop gir1.2-gdkpixbuf-2.0 gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-harfbuzz-0.0 gir1.2-pango-1.0 pango1.0-tests pango1.0-tools cairo-5c gir1.2-xkl-1.0 gir1.2-rsvg-2.0 compton compton-conf udevil udisks2 usermode upower  
+
+
+
+			
