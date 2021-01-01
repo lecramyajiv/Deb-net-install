47ec7c4cbedfac46355577daf5a488bd2639caa2
lecramyajiv <36596722+lecramyajiv@users.noreply.github.com>
2021-01-01 19:37:12 +0530

    Update and rename 08-keyring.sh to 00-keyring.sh

---
+++ 00-keyring.sh
@@ -0,0 +1,8 @@
+# This needs to run as root
+# Keyrings for apt
+
+
+#! /bin/bash
+
+
+apt-get --no-install-recommends --no-install-suggests install debian-archive-keyring debian-keyring debian-ports-archive-keyring leap-archive-keyring pkg-mozilla-archive-keyring ubuntu-archive-keyring ubuntu-cloud-keyring ubuntu-dbgsym-keyring ubuntu-keyring neurodebian-archive-keyring fasttrack-archive-keyring emdebian-archive-keyring
--- 08-keyring.sh
+++
@@ -1,9 +0,0 @@
-# This needs to run as root
-# Keyrings for apt
-
-
-#! /bin/bash
-
-
-apt-get --no-install-recommends --no-install-suggests install debian-archive-keyring debian-keyring debian-ports-archive-keyring leap-archive-keyring
-pkg-mozilla-archive-keyring ubuntu-archive-keyring ubuntu-cloud-keyring ubuntu-dbgsym-keyring ubuntu-keyring
