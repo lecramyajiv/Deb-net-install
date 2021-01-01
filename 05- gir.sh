c6a45fa4f039a39789b489c700eba058df64070a
lecramyajiv <36596722+lecramyajiv@users.noreply.github.com>
2021-01-01 15:21:14 +0530

    Update 05- gir.sh

--- 05- gir.sh
+++ 05- gir.sh
@@ -4,7 +4,7 @@
 
 #!/bin/bash
 
-apt-get --no-install-recommends --no-install-suggests install gir1.2-atk-1.0 gir1.2-freedesktop gir1.2-gdkpixbuf-2.0 gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-harfbuzz-0.0 gir1.2-pango-1.0 pango1.0-tests pango1.0-tools cairo-5c gir1.2-xkl-1.0 gir1.2-rsvg-2.0 compton compton-conf udevil udisks2 usermode upower  
+apt-get --no-install-recommends --no-install-suggests install pango1.0-tests pango1.0-tools cairo-5c gir1.2-xkl-1.0 gir1.2-rsvg-2.0 compton compton-conf udevil udisks2 usermode upower  
 
 
 
