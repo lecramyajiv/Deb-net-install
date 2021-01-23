54e0e0e82cdb31fe361a3103a11c5c718db2c873
lecramyajiv <36596722+lecramyajiv@users.noreply.github.com>
2021-01-23 21:44:11 +0530

    Update 09a-system.sh

--- 09a-system.sh
+++ 09a-system.sh
@@ -5,4 +5,4 @@
 
 #!/bin/bash
 
-apt-get --no-install-recommends --no-install-suggests install acetoneiso synaptic gparted xarchiver spacefm-gtk3 porg qps gtk-theme-switch gtk-chtheme qt5ct ntfs-3g lightdm
+apt-get --no-install-recommends --no-install-suggests install acetoneiso synaptic gparted xarchiver pcmanfm-qt qps gtk-theme-switch gtk-chtheme qt5ct ntfs-3g 
