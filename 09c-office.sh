e43f383c56cebcf656e55c22835a3229d99c2fa5
lecramyajiv <36596722+lecramyajiv@users.noreply.github.com>
2021-01-24 13:47:37 +0530

    Update 09c-office.sh

--- 09c-office.sh
+++ 09c-office.sh
@@ -3,4 +3,5 @@
 
 #!/bin/bash
 
-apt-get --no-install-recommends --no-install-suggests install qpdfview qpdfview-djvu-plugin qpdfview-pdf-poppler-plugin qpdfview-ps-plugin qpdfview-translations xchm fbreader vym pdfarranger libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-gtk3 libreoffice-gnome hunspell-en-au hunspell-en-ca hunspell-en-gb hunspell-en-med hunspell-en-us hunspell-en-za mythes-en-au mythes-en-us hyphen-en-gb hyphen-en-us
+apt-get --no-install-recommends --no-install-suggests install qpdfview qpdfview-djvu-plugin qpdfview-pdf-poppler-plugin qpdfview-ps-plugin qpdfview-translations xchm fbreader vym pdfarranger libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-gtk3 libreoffice-gnome hunspell-en-au hunspell-en-ca hunspell-en-gb hunspell-en-med hunspell-en-us hunspell-en-za mythes-en-au mythes-en-us hyphen-en-gb hyphen-en-us ibus ibus-anthy ibus-array ibus-cangjie ibus-clutter ibus-gtk3 ibus-hangul ibus-keyman ibus-kmfl ibus-m17n ibus-mozc im-config icc-profiles icc-profiles-free
+
