3c680616e736244d799493dccb4508d4307f5866
lecramyajiv <36596722+lecramyajiv@users.noreply.github.com>
2021-02-12 20:55:15 +0530

    Update 01-font.sh

--- 01-font.sh
+++ 01-font.sh
@@ -4,5 +4,4 @@
 
 #!/bin/bash 
 
-apt-get --no-install-recommends --no-install-suggests install fonts-indic fonts-noto fonts-pagul fonts-pecita fonts-opensymbol fonts-recommended fonts-rufscript fonts-sil-andika fonts-sil-annapurna fonts-sil-charis fonts-sil-galatia fonts-sil-gentiumplus fonts-sil-doulos fonts-terminus-otb fonts-terminus fonts-adf-baskervald fonts-gfs-baskerville fonts-century-catalogue fonts-ebgaramond fonts-ebgaramond-extra fonts-eeyek fonts-johnsmith-induni t1-cyrillic ttf-bitstream-vera ttf-mscorefonts-installer xfstt x11-xfs-utils
- 
+apt-get --no-install-recommends --no-install-suggests install onts-indic fonts-noto fonts-pagul fonts-pecita fonts-opensymbol fonts-recommended fonts-rufscript fonts-sil-andika fonts-sil-annapurna fonts-sil-charis fonts-sil-galatia fonts-sil-gentiumplus fonts-sil-doulos fonts-terminus-otb fonts-terminus fonts-adf-baskervald fonts-gfs-baskerville fonts-century-catalogue fonts-ebgaramond fonts-ebgaramond-extra fonts-eeyek fonts-johnsmith-induni t1-cyrillic ttf-bitstream-vera ttf-mscorefonts-installer fonts-lexi-gulim fonts-lexi-saebom fonts-unfonts-core fonts-unfonts-extra fonts-nanum fonts-nanum-extra fonts-moe-standard-kai fonts-moe-standard-song fonts-arphic-uming fonts-dejima-mincho fonts-ipaexfont fonts-ipafont-nonfree-jisx0208 fonts-migmix fonts-noto fonts-noto-color-emoji xfstt x11-xfs-utils
